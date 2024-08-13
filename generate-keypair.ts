e39cac8ea1c395f882b06b9e36822d06f881683b
Mateusz Zając <matzayonc@gmail.com>
2024-08-13 18:54:39 +0200

    loading keypairs

---
+++ generate-keypair.ts
@@ -0,0 +1,9 @@
+import { addKeypairToEnvFile } from "@solana-developers/helpers";
+import { Keypair } from "@solana/web3.js";
+const keypair = Keypair.generate();
+
+addKeypairToEnvFile(keypair, "SECRET_KEY");
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
