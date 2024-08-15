c45dfeb7cd64cea709e3ce04605f8f19262ef355
Mateusz Zając <matzayonc@gmail.com>
2024-08-15 18:39:44 +0200

    check by address

--- check-balance.ts
+++ check-balance.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { airdropIfRequired, getKeypairFromEnvironment } from "@solana-developers/helpers";
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
 
 const keypair = getKeypairFromEnvironment("SECRET_KEY");
 
@@ -9,7 +9,9 @@ const connection = new Connection(clusterApiUrl("devnet"));
 const lamports = 7 * (10 ** 8); // 0.7 SOL
 // await airdropIfRequired(connection, keypair.publicKey, lamports, lamports);
 
-const balance = await connection.getBalance(keypair.publicKey);
-console.log('Checking balance of address:', keypair.publicKey.toBase58(), 'Balance:', balance);
+const address = new PublicKey('2XCBRmosLs2KovcRXe2Kf6yp9J3M2SAiysdMHLD7FnqH')
+
+const balance = await connection.getBalance(address);
+console.log('Checking balance of address:', address.toBase58(), 'Balance:', balance);
 console.log('Balance in SOL:', balance / (10 ** 9));
 
